f3ef8f1ddf64e48f54b0b4c073747ca1893a126b
OtanCZ <otto2004@seznam.cz>
2023-02-20 14:37:38 +0100

    Window achieved

---
+++ README.md
@@ -0,0 +1,3 @@
+# That plane 2d topdown shoot em' up game which name idk
+## inspiration img: 
+![inspiration](https://www.retrogamer.net/wp-content/uploads/2013/10/raidenii.png)
--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+from pygame.locals import *
 
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 400
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
 
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+    def on_loop(self):
+        pass
+    def on_render(self):
+        pass
+    def on_cleanup(self):
+        pygame.quit()
 
-# Press the green button in the gutter to run the script
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+
+if __name__ == "__main__" :
+    theApp = App()
+    theApp.on_execute()
+
