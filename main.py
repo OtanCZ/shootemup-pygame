e40f336c9ea35ff2becc6b793c472fe380086eea
OtanCZ <otto2004@seznam.cz>
2023-04-24 13:57:17 +0200

    difficulty increasing by time played, added sounds and music

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from random import random
 from threading import Timer
 
@@ -14,22 +15,26 @@ class App:
         self.size = self.width, self.height = 600, 400
         self.projectiles = []
         self.enemies = []
-        self.enemySpawnerTimer = Timer(1, self.spawnEnemy)
         self.player = None
+        self.score = 0
+        self.difficulty = 3
+        self.enemySpawnerTimer = Timer(self.difficulty, self.spawnEnemy)
 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
-        self.player = DestructibleEntity(0, 0, 0, 0, pygame.image.load("images/player.png"), 0, 3, 3, 0.5)
+        self.player = DestructibleEntity(0, 0, 0, 0, pygame.image.load("resources/images/player.png"), 0, 3, 3, 0.5)
         self.player.x = self.width / 2 - self.player.texture.get_width() / 2
         self.player.y = self.height - self.player.texture.get_height()
         self.enemySpawnerTimer.start()
-
-
+        pygame.mixer.music.load("resources/sounds/Numb.mid")
+        pygame.mixer.music.play(-1)
     def spawnEnemy(self):
-        self.enemies.append(DestructibleEntity(random()*self.width, -16, 0, 0.08, pygame.image.load("images/player.png"), 180, 3, 3, 0.1))
-        self.enemySpawnerTimer = Timer(0.5, self.spawnEnemy)
+        tankiness = random()*5
+        self.enemies.append(DestructibleEntity(random()*(self.width-32), -16, 0, 0.16-0.025*tankiness, pygame.image.load("resources/images/player.png"), 180, tankiness, tankiness, 0.1))
+
+        self.enemySpawnerTimer = Timer(self.difficulty, self.spawnEnemy)
         self.enemySpawnerTimer.start()
 
     def on_event(self, event):
@@ -40,17 +45,20 @@ class App:
             if event.key == pygame.K_ESCAPE:
                 self._running = False
             if event.key == pygame.K_UP:
-                self.player.y_velocity = -0.3
+                self.player.y_velocity = -0.4
             if event.key == pygame.K_DOWN:
-                self.player.y_velocity = 0.3
+                self.player.y_velocity = 0.4
             if event.key == pygame.K_LEFT:
-                self.player.x_velocity = -0.3
+                self.player.x_velocity = -0.4
             if event.key == pygame.K_RIGHT:
-                self.player.x_velocity = 0.3
+                self.player.x_velocity = 0.4
             if event.key == pygame.K_SPACE:
-                self.projectiles.append(ProjectileEntity(self.player.x + self.player.texture.get_width() / 2, self.player.y, 0, -1, pygame.image.load("images/bullet.png").convert_alpha(), 0, 1))
+                self.projectiles.append(ProjectileEntity(self.player.x + self.player.texture.get_width() / 2, self.player.y, 0, -1, pygame.image.load("resources/images/bullet.png").convert_alpha(), 0, 1))
+                pygame.mixer.Sound.play(pygame.mixer.Sound("resources/sounds/laserShoot.wav"))
+
+
             if event.key == pygame.K_BACKSPACE:
-                self.enemies.append(DestructibleEntity(self.player.x + self.player.texture.get_width() / 2, self.player.y, 0, 0, pygame.image.load("images/player.png").convert_alpha(), 0, 3, 3, 0.5))
+                self.enemies.append(DestructibleEntity(self.player.x + self.player.texture.get_width() / 2, self.player.y, 0, 0, pygame.image.load("resources/images/player.png").convert_alpha(), 0, 3, 3, 0.5))
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 self.player.y_velocity = 0
@@ -58,6 +66,7 @@ class App:
                 self.player.x_velocity = 0
 
     def on_loop(self):
+        self.difficulty = 3 - round(pygame.time.get_ticks() / 250000, 3)
         self.player.update()
         for projectile in self.projectiles:
             projectile.update()
@@ -66,20 +75,26 @@ class App:
             enemy.update()
             if enemy.y > self.height:
                 self.enemies.remove(enemy)
+                self.player.hurt(1)
 
             if self.player.x < enemy.x < self.player.x + self.player.texture.get_width() and self.player.y < enemy.y < self.player.y + self.player.texture.get_height():
                 self.player.hurt(1)
                 enemy.hurt(1)
+                pygame.mixer.Sound.play(pygame.mixer.Sound("resources/sounds/hitHurt.wav"))
 
             for projectile in self.projectiles:
                 if enemy.x < projectile.x < enemy.x + enemy.texture.get_width() and enemy.y < projectile.y < enemy.y + enemy.texture.get_height():
                     enemy.hurt(projectile.damage)
+                    pygame.mixer.Sound.play(pygame.mixer.Sound("resources/sounds/hitHurt.wav"))
                     self.projectiles.remove(projectile)
 
             if enemy.health <= 0:
                 self.enemies.remove(enemy)
+                pygame.mixer.Sound.play(pygame.mixer.Sound("resources/sounds/explosion.wav"))
+                self.score = self.score + 1
 
             if self.player.health <= 0:
+                pygame.mixer.Sound.play(pygame.mixer.Sound("resources/sounds/explosion.wav"))
                 self._running = False
 
         for projectile in self.projectiles:
@@ -93,6 +108,12 @@ class App:
             projectile.draw(self._display_surf)
         for enemy in self.enemies:
             enemy.draw(self._display_surf)
+
+        font = pygame.font.SysFont("monospace", 18)
+        text = font.render('Score: ' + str(self.score) + " | Health: " + str(self.player.health) + " | Difficulty: " + str(self.difficulty), True, 'black')
+        textRect = text.get_rect()
+        textRect.center = (self.width/2, self.height-9)
+        self._display_surf.blit(text, textRect)
         pygame.display.flip()
 
     def on_cleanup(self):
